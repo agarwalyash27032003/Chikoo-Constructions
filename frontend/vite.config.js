3a6b4ec2d642166ef189b4125de222ce00a40c5b
Yash Agarwal <agarwalyash27032003@gmail.com>
2026-03-19 02:26:52 +0530

    New folders

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),{
+      name: "copy-redirects",
+      closeBundle() {
+        copyFileSync("public/_redirects", "dist/_redirects");
+      }
+    }],
   server: {
     proxy: {
       "/api": {
